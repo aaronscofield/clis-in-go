d19720fb6e374a3df4729d9a60ccc33e2a72f5e2
Aaron Scofield <aaron.scofield@icloud.com>
2025-04-01 17:13:08 -0400

    chapter 8 add todoClient

---
+++ chapter8/todoClient/main.go
@@ -0,0 +1,13 @@
+/*
+Copyright © 2025 Aaron Scofield
+Copyrights apply to this source code.
+Check LICENSE for details.
+
+*/
+package main
+
+import "clis-in-go/chapter8/todoClient/cmd"
+
+func main() {
+	cmd.Execute()
+}
